6aa1890f5e3af31c99eb84d20ce05dd870dd05d3
Mallikarjun Neela <117260058+mallikarjunneela@users.noreply.github.com>
2022-11-19 21:08:16 +0530

    Update index.js

--- src/components/HistoryItem/index.js
+++ src/components/HistoryItem/index.js
@@ -1,38 +1,33 @@
-.navBar {
-  background-color: #3367d6;
-  display: flex;
-  align-items: center;
-  padding: 10px 30px;
+import React from 'react'
+
+import './index.css'
+
+const HistoryItem = props => {
+  const {historyItem, deleteItem} = this.props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = historyItem
+
+  const handleDelete = () => {
+    deleteItem(id)
+  }
+
+  return (
+    <li>
+      <div className="list-container">
+        <p>{timeAccessed}</p>
+        <img src={logoUrl} alt="domain logo" />
+        <p>{title}</p>
+        <p>{domainUrl}</p>
+      </div>
+      <div>
+        <button onClick={handleDelete}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
+  )
 }
 
-.searchBox {
-  background-color: #2850a7;
-  margin: auto;
-  width: 400px;
-  height: 40px;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  border: 1px solid #6697ff;
-  box-sizing: 5px;
-}
-
-.searchBox input {
-  background-color: transparent;
-  color: #ffffff;
-  border: 0;
-  outline: none;
-  width: 400px;
-  height: 40px;
-}
-
-.search-results-container {
-  background-color: #ffffff;
-}
-
-.noItems {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 90vh;
-}
+export default HistoryItem
