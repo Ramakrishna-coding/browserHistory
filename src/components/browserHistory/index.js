ff7c922c80c07d4deeb61b18ff117741f6df69f0
Mallikarjun Neela <117260058+mallikarjunneela@users.noreply.github.com>
2022-11-24 18:29:26 +0530

    Update index.js

--- src/components/browserHistory/index.js
+++ src/components/browserHistory/index.js
@@ -1,124 +1,89 @@
-import React, {Component} from 'react'
+import {Component} from 'react'
+
+import HistoryItem from '../HistoryItem'
 
 import './index.css'
 
-const initialHistoryList = [
-  {
-    id: 0,
-    timeAccessed: '07:45 PM',
-    logoUrl: 'https://assets.ccbp.in/frontend/react-js/instagram-img.png',
-    title: 'Instagram',
-    domainUrl: 'instagram.com',
-  },
-  {
-    id: 1,
-    timeAccessed: '05:45 PM',
-    logoUrl: 'https://assets.ccbp.in/frontend/react-js/twitter-img.png',
-    title: 'Twitter. It’s what’s happening / Twitter',
-    domainUrl: 'twitter.com',
-  },
-  {
-    id: 2,
-    timeAccessed: '04:35 PM',
-    logoUrl: 'https://assets.ccbp.in/frontend/react-js/facebook-img.png',
-    title: 'Facebook – log in or sign up',
-    domainUrl: 'facebook.com',
-  },
-  {
-    id: 3,
-    timeAccessed: '04:25 PM',
-    logoUrl: 'https://assets.ccbp.in/frontend/react-js/linkedin-img.png',
-    title: 'LinkedIn: Log In or Sign Up',
-    domainUrl: 'linkedin.com',
-  },
-  {
-    id: 4,
-    timeAccessed: '04:00 PM',
-    logoUrl: 'https://assets.ccbp.in/frontend/react-js/hashnode-img.png',
-    title: 'Hashnode: Everything you need to start blogging as a developer!',
-    domainUrl: 'hashnode.com',
-  },
-  {
-    id: 5,
-    timeAccessed: '03:25 PM',
-    logoUrl: 'https://assets.ccbp.in/frontend/react-js/github-img.png',
-    title: 'GitHub: Where the world builds software · GitHub',
-    domainUrl: 'github.com',
-  },
+class browserHistory extends Component {
+  state = {
+    searchInput: '',
+    historyList: [],
+  }
 
-  {
-    id: 6,
-    timeAccessed: '02:45 PM',
-    logoUrl: 'https://assets.ccbp.in/frontend/react-js/react-img.png',
-    title: 'React – A JavaScript library for building user interfaces',
-    domainUrl: 'reactjs.org',
-  },
-  {
-    id: 7,
-    timeAccessed: '01:25 PM',
-    logoUrl: 'https://assets.ccbp.in/frontend/react-js/stackoverflow-img.png',
-    title: 'Stack Overflow - Where Developers Learn, Share, & Build Careers',
-    domainUrl: 'stackoverflow.com',
-  },
+  componentDidMount() {
+    const {initialHistoryList} = this.props
+    this.setState({historyList: initialHistoryList})
+  }
 
-  {
-    id: 8,
-    timeAccessed: '09:25 AM',
-    logoUrl: 'https://assets.ccbp.in/frontend/react-js/gmail-img.png',
-    title: 'Gmail',
-    domainUrl: 'mail.google.com',
-  },
-  {
-    id: 9,
-    timeAccessed: '09:00 AM',
-    logoUrl: 'https://assets.ccbp.in/frontend/react-js/google-img.png',
-    title: 'Google',
-    domainUrl: 'google.com',
-  },
-]
+  filterHistoryList = () => {
+    const {searchInput, historyList} = this.state
+    const updatedHistoryList = historyList.filter(eachHistory =>
+      eachHistory.title.toLowerCase().includes(searchInput.toLowerCase()),
+    )
 
-export default class browserHistory Component{
-    state = {
-        historyList: initialHistoryList
-    }
+    return updatedHistoryList
+  }
 
-    getSearchResults = (event) => {
-        const searchTerm = event.target.value
-        const filteredItems = initialHistoryList.filter(each => each.title.toLowerCase().includes(searchTerm.toLowerCase()),
-        ) 
-        this.setState({historyList: filteredItems})
-        console.log(filteredItems)
-    }
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
 
-    deleteItem=(id) =>{
-        const {historyList} = this.state
-        const filteredList = historyList.filter(each => each.id !== id)
-        this.setState({historyList: filteredList})
+  onDeleteHistory = id => {
+    const {historyList} = this.state
+    const updatedHistoryList = historyList.filter(
+      eachHistory => eachHistory.id !== id,
+    )
 
-    }
-    render(){
-        const {historyList} = this.state
-        return(
-            <div>
-            <div className="navBar">
-            <img src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
-            alt="app logo" />
-            <div className="searchBox">
-                <img src="https://assets.ccbp.in/frontend/react-js/search-img.png"
-                alt="search" />
-                <input type="search" placeholder="Search history" onChange={this.getSearchResults} />
+    this.setState({historyList: updatedHistoryList})
+  }
+
+  render() {
+    const {searchInput} = this.state
+    const filteredHistoryList = this.filterHistoryList()
+
+    return (
+      <div className="app-container">
+        <div className="responsive-container">
+          <div className="header-container">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
+              alt="app logo"
+            />
+            <div className="search-container">
+              <div className="search-icon-container">
+                <img
+                  src="https://assets.ccbp.in/frontend/react-js/search-img.png"
+                  alt="search"
+                />
+              </div>
+              <div className="search-input-container">
+                <input
+                  type="search"
+                  className="search-input"
+                  placeholder="Search history"
+                  value={searchInput}
+                  onChange={this.onChangeSearchInput}
+                />
+              </div>
             </div>
-            </div>
-            <div className="search-results-container">
-            {historyList.length<1 ? <div className="noItems"><p>There is no history to show</p></div> : <ul>
-                {historyList.map(each => ( <HistoryItem key={each.id} HistoryItem={each}
-                deleteItem={this.deleteItem} />
-                ))}
-            </ul>
-                )}
-
-            </div>
-            </div>
-        )
-    }
+          </div>
+        </div>
+        {filteredHistoryList.length === 0 ? (
+          <p className="no-history">There is no history to show</p>
+        ) : (
+          <ul className="history-container">
+            {filteredHistoryList.map(eachHistory => (
+              <HistoryItem
+                key={eachHistory.id}
+                historyDetails={eachHistory}
+                onDeleteHistory={this.onDeleteHistory}
+              />
+            ))}
+          </ul>
+        )}
+      </div>
+    )
+  }
 }
+
+export default browserHistory
